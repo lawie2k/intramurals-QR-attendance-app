ef2b901f7975fd8702266c9c70ffa02cc26fa96a
art laurence siojo <149293233+lawie2k@users.noreply.github.com>
2025-09-21 22:40:38 +0800

    geting the files ready

--- qr-attendance-app/screens/scanSuccess.tsx
+++ qr-attendance-app/screens/scanSuccess.tsx
@@ -1,11 +1,12 @@
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {View, Text, TouchableOpacity} from "react-native";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
+import {useNavigation} from "@react-navigation/native";
 
 export default function ScanSuccess () {
+    const navigation = useNavigation();
     return (
-        <SafeAreaProvider>
-            <SafeAreaView>
-
-            </SafeAreaView>
-        </SafeAreaProvider>
+       //i butang diria ang code
     )
 }
