8e90e61e6e253292c78b3fc49a8e7c498ab47274
Kiks <g.sangil.143528.tc@umindanao.edu.ph>
2025-09-23 01:45:38 +0800

    AttendanceSangil

--- qr-attendance-admin/src/tabs/attendance.tsx
+++ qr-attendance-admin/src/tabs/attendance.tsx
@@ -2,7 +2,25 @@ import React from "react";
 
 export default function Attendance() {
 	return (
-		<div>Attendance</div>
+		<div className="">
+			<h1 className="mt-[20px] text-[20px]"><span className="font-bold text-[36px] ml-[35px]">Attendance </span></h1>
+		<div>
+			<div className="w-[1000px] h-[650px] mt-[30px] ml-[40px] flex flex-col justify-between	 ">
+				
+				<div className="flex justify-between">
+				<div className="w-[250px] h-[250px] bg-[#D9D9D9] "></div>
+				<div className="w-[250px] h-[250px] bg-[#D9D9D9] "></div>
+				<div className="w-[250px] h-[250px] bg-[#D9D9D9] "></div>
+				</div>
+				<div className="flex justify-between">
+				<div className="w-[400px] h-[300px] bg-[#D9D9D9]"></div>
+				<div className="w-[550px] h-[300px] bg-[#D9D9D9]"></div>
+				</div>
+				
+			</div>
+			</div>
+		</div>
+		
 	);
 }
 
